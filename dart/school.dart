5829aa8ca6955cf2149a7a595fdade0660be33ee
qacl <qacl@users.noreply.github.com>
2018-10-11 08:32:37 -0700

    Create school.dart

---
+++ dart/school.dart
@@ -0,0 +1,4 @@
+// *****      Port of the 2016 Girls Who Code CS Impact Project to Dart       ***** //
+// This project was canceled due to lack of teachers, so I am going to rewrite it in Dart
+
+import "dart:html";
